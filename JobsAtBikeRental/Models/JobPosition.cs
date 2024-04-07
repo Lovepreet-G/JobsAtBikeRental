58b679b5e0bf9283029b0ca71d55f64cf3234a46
SurbhiSinghania13 <surbhisinghania404@gmail.com>
2024-04-06 21:27:56 -0400

    applicant crud completed

---
+++ JobsAtBikeRental/Models/JobPosition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobsAtBikeRental.Models
+{
+    public class JobPosition
+    {
+        [Key]
+        public int JobPositionId { get; set; }
+        public string JobTitle { get; set; }
+
+        public string JobLocation { get; set; }
+    }
+    public class JobPositionDTO
+    {
+        public int JobPositionId { get; set; }
+        public string JobTitle { get; set; }
+        public string JobLocation { get; set; }
+
+
+    }
+}
