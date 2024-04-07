28c18179043e6cf1d7163f629d2d81325dfe7720
Lovepreet <gillpreetsingh35@gmail.com>
2024-04-06 21:23:24 -0400

    first commit

--- JobsAtBikeRental/Controllers/HomeController.cs
+++ JobsAtBikeRental/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace JobsAtBikeRental.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home";
 
             return View();
         }
